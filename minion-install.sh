b10ee0d462c6594ffd8883a21ce1d6f01358ab7f
kevinwood75 <kevin.wood75@gmail.com>
2018-03-18 21:42:29 -0400

    added logic for ubuntu

--- minion-install.sh
+++ minion-install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [[ $(uname -r | cut -d\. -f1) -eq 4 ]];then
+    wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | apt-key add -
+    apt-get -y update
+    apt-get -y install salt-minion
+    sed -e 's/#master: salt/master: saltmaster01.woodez.net/g' -i /etc/salt/minion
+    systemctl restart salt-minion
+    exit 0
+fi
+
 yum -y install https://repo.saltstack.com/yum/redhat/salt-repo-latest-2.el7.noarch.rpm
 yum clean expire-cache
 yum -y install salt-minion
