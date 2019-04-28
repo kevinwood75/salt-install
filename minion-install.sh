da615391335336c456b3a809549563cfd34b0a33
Kevin Wood <kevin.wood75@gmail.com>
2019-04-28 08:05:32 -0400

    added pip

--- minion-install.sh
+++ minion-install.sh
@@ -12,5 +12,8 @@ fi
 yum -y install https://repo.saltstack.com/yum/redhat/salt-repo-latest-2.el7.noarch.rpm
 yum clean expire-cache
 yum -y install salt-minion
+yum install epel
+yum install python2-pip
+pip install certifi
 sed -e 's/#master: salt/master: saltmaster01.woodez.net/g' -i /etc/salt/minion
 service salt-minion restart
