40667593b8ffb41d04731b41db700722e47d66a2
Kevin Wood <kevin.wood75@gmail.com>
2024-01-13 23:30:01 -0500

    test

--- minion-install.sh
+++ minion-install.sh
@@ -18,6 +18,15 @@ elif [[ $(cat /etc/redhat-release  | awk '{print $4}' | cut -d\. -f1) -eq 8 ]];t
     systemctl restart salt-minion
     systemctl enable salt-minion
     exit 0
+elif [[ $(cat /etc/redhat-release  | awk '{print $4}' | cut -d\. -f1) -eq 9 ]];then
+    rpm --import https://repo.saltproject.io/salt/py3/redhat/9/x86_64/3005/SALTSTACK-GPG-KEY2.pub
+    curl -fsSL https://repo.saltproject.io/salt/py3/redhat/9/x86_64/3005.repo | sudo tee /etc/yum.repos.d/salt.repo
+    yum clean expire-cache
+    yum -y install salt-minion
+    sed -e 's/#master: salt/master: saltmaster01.woodez.net/g' -i /etc/salt/minion
+    systenctl restart salt-minion
+    systemctl enable salt-minion
+    exit 0
 else
     rpm --import https://repo.saltproject.io/py3/redhat/7/x86_64/latest/SALTSTACK-GPG-KEY.pub
     curl -fsSL https://repo.saltproject.io/py3/redhat/7/x86_64/latest.repo | sudo tee /etc/yum.repos.d/salt.repo
